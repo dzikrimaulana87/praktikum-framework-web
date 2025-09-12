55ab2087762dda612fc6cb5dfcbfd10e373e3568
Dzikri Maulana <45265319+dzikrimaulana87@users.noreply.github.com>
2025-09-12 08:21:05 +0700

    pertemuan 2

---
+++ resources/views/about.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum impedit, officia voluptas in cum rerum soluta quod
+    nulla non! Illo.
+
+
+    <a href="{{ route('contact.page') }}">Halaman Kontak</a>
+
+</body>
+
+</html>
